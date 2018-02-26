sarahkha/RMG-Py

Add hydrogen bond type
hydrogen bonds correspond to the string 'H' and the order 0

rmgpy/molecule/group.py
@@ -619,6 +619,8 @@ class GroupBond(Edge):
                 values.append('T')
             elif value == 1.5:
                 values.append('B')
+            elif value == 0:
+                values.append('H')
             else:
                 raise TypeError('Bond order number {} is not hardcoded as a string'.format(value))
         return values
@@ -638,6 +640,8 @@ class GroupBond(Edge):
                 values.append(3)
             elif value == 'B':
                 values.append(1.5)
+            elif value == 'H':
+                values.append(0)
             else:
                 # try to see if an float disguised as a string was input by mistake
                 try:
@@ -722,6 +726,21 @@ class GroupBond(Edge):
         else:
             return abs(self.order[0]-1.5) <= 1e-9 and len(self.order) == 1
 
+    def isHydrogenBond(self, wildcards = False):
+        """
+        Return ``True`` if the bond represents a triple bond or ``False`` if
+        not. If `wildcards` is ``False`` we return False anytime there is more
+        than one bond order, otherwise we return ``True`` if any of the options
+        are triple.
+        """
+        if wildcards:
+            for order in self.order:
+                if abs(order) <= 1e-9:
+                    return True
+            else: return False
+        else:
+            return abs(self.order[0]) <= 1e-9 and len(self.order) == 1
+        
     def __changeBond(self, order):
         """
         Update the bond group as a result of applying a CHANGE_BOND action,

ReactionMechanismGenerator/molecule

group.py
@@ -627,6 +627,8 @@ class GroupBond(Edge):
                 values.append('T')
             elif value == 1.5:
                 values.append('B')
+            elif value == 0:
+                values.append('H')
             else:
                 raise TypeError('Bond order number {} is not hardcoded as a string'.format(value))
         return values
@@ -646,6 +648,8 @@ class GroupBond(Edge):
                 values.append(3)
             elif value == 'B':
                 values.append(1.5)
+            elif value == 'H':
+                values.append(0)
             else:
                 # try to see if an float disguised as a string was input by mistake
                 try:
@@ -730,6 +734,21 @@ class GroupBond(Edge):
         else:
             return abs(self.order[0]-1.5) <= 1e-9 and len(self.order) == 1
 
+    def isHydrogenBond(self, wildcards = False):
+        """
+        Return ``True`` if the bond represents a hydrogen bond or ``False`` if
+        not. If `wildcards` is ``False`` we return False anytime there is more
+        than one bond order, otherwise we return ``True`` if any of the options
+        are hydrogen bonds.
+        """
+        if wildcards:
+            for order in self.order:
+                if abs(order) <= 1e-9:
+                    return True
+            else: return False
+        else:
+            return abs(self.order[0]) <= 1e-9 and len(self.order) == 1
+        
     def __changeBond(self, order):
         """
         Update the bond group as a result of applying a CHANGE_BOND action,