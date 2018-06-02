alongd/RMG-Py

Added resonance transformations specific for atoms with lone pairs
- Modified lone_pair_radical_resonance_structures
- Added lone_pair_multiple_bond_resonance_structures
- Added lone_pair_radical_multiple_bond_resonance_structures
- Modified N5ddc_N5tc_resonance_structures
- Extended docstrings
- Added respective changes to pathfinder.py

rmgpy/molecule/pathfinder.pxd
@@ -44,8 +44,18 @@ cpdef list add_inverse_allyls(list path)
 
 cpdef dict compute_atom_distance(list atom_indices, Molecule mol)
 
-cpdef list findAllDelocalizationPaths(Atom atom1)
+cpdef list find_allyl_delocalization_paths(Atom atom1)
 
-cpdef list findAllDelocalizationPathsLonePairRadical(Atom atom1)
+cpdef list find_lone_pair_radical_delocalization_paths(Atom atom1)
 
-cpdef list findAllDelocalizationPathsN5dd_N5ts(Atom atom1)
+cpdef list find_lone_pair_multiple_bond_delocalization_paths(Atom atom1)
+
+cpdef list find_lone_pair_radical_multiple_bond_delocalization_paths(Atom atom1)
+
+cpdef list find_N5ddc_N5tc_delocalization_paths(Atom atom1)
+
+cpdef list find_N5dc_delocalization_paths(Atom atom1)
+
+cpdef bint is_NOS_able_to_gain_lone_pair(Atom atom)
+
+cpdef bint is_NOS_able_to_loose_lone_pair(Atom atom)

rmgpy/molecule/resonance.pxd
@@ -32,15 +32,21 @@ cpdef list populate_resonance_algorithms(dict features=?)
 
 cpdef dict analyze_molecule(Molecule mol)
 
-cpdef list generate_resonance_structures(Molecule mol, bint clarStructures=?, bint keepIsomorphic=?)
+cpdef list generate_resonance_structures(Molecule mol, bint clar_structures=?, bint keep_isomorphic=?, bint filter_structures=?)
 
-cpdef list _generate_resonance_structures(list molList, list methodList, bint keepIsomorphic=?, bint copy=?)
+cpdef list _generate_resonance_structures(list mol_list, list method_list, bint keep_isomorphic=?, bint copy=?)
 
-cpdef list generate_adjacent_resonance_structures(Molecule mol)
+cpdef list generate_ally_delocalization_resonance_structures(Molecule mol)
 
 cpdef list generate_lone_pair_radical_resonance_structures(Molecule mol)
 
-cpdef list generate_N5dd_N5ts_resonance_structures(Molecule mol)
+cpdef list generate_lone_pair_multiple_bond_resonance_structures(Molecule mol)
+
+cpdef list generate_lone_pair_radical_multiple_bond_resonance_structures(Molecule mol)
+
+cpdef list generate_N5ddc_N5tc_resonance_structures(Molecule mol)
+
+cpdef list generate_N5dc_resonance_structures(Molecule mol)
 
 cpdef list generate_isomorphic_resonance_structures(Molecule mol)
 

ReactionMechanismGenerator/molecule

pathfinder.pxd
@@ -44,8 +44,18 @@ cpdef list add_inverse_allyls(list path)
 
 cpdef dict compute_atom_distance(list atom_indices, Molecule mol)
 
-cpdef list findAllDelocalizationPaths(Atom atom1)
+cpdef list find_allyl_delocalization_paths(Atom atom1)
 
-cpdef list findAllDelocalizationPathsLonePairRadical(Atom atom1)
+cpdef list find_lone_pair_radical_delocalization_paths(Atom atom1)
 
-cpdef list findAllDelocalizationPathsN5dd_N5ts(Atom atom1)
+cpdef list find_lone_pair_multiple_bond_delocalization_paths(Atom atom1)
+
+cpdef list find_lone_pair_radical_multiple_bond_delocalization_paths(Atom atom1)
+
+cpdef list find_N5ddc_N5tc_delocalization_paths(Atom atom1)
+
+cpdef list find_N5dc_delocalization_paths(Atom atom1)
+
+cpdef bint is_NOS_able_to_gain_lone_pair(Atom atom)
+
+cpdef bint is_NOS_able_to_lose_lone_pair(Atom atom)

resonance.pxd
@@ -32,15 +32,21 @@ cpdef list populate_resonance_algorithms(dict features=?)
 
 cpdef dict analyze_molecule(Molecule mol)
 
-cpdef list generate_resonance_structures(Molecule mol, bint clarStructures=?, bint keepIsomorphic=?)
+cpdef list generate_resonance_structures(Molecule mol, bint clar_structures=?, bint keep_isomorphic=?, bint filter_structures=?)
 
-cpdef list _generate_resonance_structures(list molList, list methodList, bint keepIsomorphic=?, bint copy=?)
+cpdef list _generate_resonance_structures(list mol_list, list method_list, bint keep_isomorphic=?, bint copy=?)
 
-cpdef list generate_adjacent_resonance_structures(Molecule mol)
+cpdef list generate_allyl_delocalization_resonance_structures(Molecule mol)
 
 cpdef list generate_lone_pair_radical_resonance_structures(Molecule mol)
 
-cpdef list generate_N5dd_N5ts_resonance_structures(Molecule mol)
+cpdef list generate_lone_pair_multiple_bond_resonance_structures(Molecule mol)
+
+cpdef list generate_lone_pair_radical_multiple_bond_resonance_structures(Molecule mol)
+
+cpdef list generate_N5ddc_N5tc_resonance_structures(Molecule mol)
+
+cpdef list generate_N5dc_resonance_structures(Molecule mol)
 
 cpdef list generate_isomorphic_resonance_structures(Molecule mol)
 