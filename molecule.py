alongd/RMG-Py

Added a logSpecies option to molecule.update()
To pass on to updateAtomTypes() for better control.
Useful for generating resonance structures.

rmgpy/molecule/molecule.py
@@ -829,7 +829,7 @@ class Molecule(Graph):
         """
         return self.sortVertices()
 
-    def update(self):
+    def update(self, log_species_while_updating_atom_types=True):
         """
         Update connectivity values, atom types of atoms.
         Update multiplicity, and sort atoms using the new
@@ -839,7 +839,7 @@ class Molecule(Graph):
         for atom in self.atoms:
             atom.updateCharge()
 
-        self.updateAtomTypes()
+        self.updateAtomTypes(logSpecies=log_species_while_updating_atom_types)
         self.updateMultiplicity()
         self.sortVertices()
 

ReactionMechanismGenerator/molecule

molecule.py
@@ -879,7 +879,7 @@ class Molecule(Graph):
         for index, vertex in enumerate(self.vertices):
             vertex.sortingLabel = index
 
-    def update(self):
+    def update(self, log_species=True):
         """
         Update connectivity values, atom types of atoms.
         Update multiplicity, and sort atoms using the new
@@ -889,7 +889,7 @@ class Molecule(Graph):
         for atom in self.atoms:
             atom.updateCharge()
 
-        self.updateAtomTypes()
+        self.updateAtomTypes(logSpecies=log_species)
         self.updateMultiplicity()
         self.sortAtoms()
         self.identifyRingMembership()