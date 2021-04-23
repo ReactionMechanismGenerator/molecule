davidfarinajr/RMG-Py

added is_bromine function to Atom class

rmgpy/molecule/molecule.py
@@ -390,6 +390,13 @@ class Atom(Vertex):
         """
         return self.element.number == 17
 
+    def is_bromine(self):
+        """
+        Return ``True`` if the atom represents a fluorine atom or ``False`` if
+        not.
+        """
+        return self.element.number == 35
+
     def is_iodine(self):
         """
         Return ``True`` if the atom represents an iodine atom or ``False`` if

ReactionMechanismGenerator/molecule

molecule.py
@@ -397,6 +397,13 @@ class Atom(Vertex):
         """
         return self.element.number == 17
 
+    def is_bromine(self):
+        """
+        Return ``True`` if the atom represents a bromine atom or ``False`` if
+        not.
+        """
+        return self.element.number == 35
+
     def is_iodine(self):
         """
         Return ``True`` if the atom represents an iodine atom or ``False`` if