ReactionMechanismGenerator/RMG-Py

Add unit test for fused aromatic with more than 2 shared atoms

rmgpy/molecule/moleculeTest.py
@@ -1988,6 +1988,13 @@ multiplicity 2
         self.assertEqual(len(aromaticAtoms), 0)
         self.assertEqual(len(aromaticBonds), 0)
 
+    def testAromaticityPerceptionBenzoNaphthalene(self):
+        """Test aromaticity perception via getAromaticRings for benzo naphthalene with multiple fused bonds."""
+        mol = Molecule(SMILES='c1cc2ccc3ccc(c1)c2c3')
+        aromaticAtoms, aromaticBonds = mol.getAromaticRings()
+        self.assertEqual(len(aromaticAtoms), 1)
+        self.assertEqual(len(aromaticBonds), 1)
+
     def testArylRadicalTrue(self):
         """Test aryl radical perception for phenyl radical."""
         mol = Molecule(SMILES='[c]1ccccc1')

ReactionMechanismGenerator/molecule

moleculeTest.py
@@ -2501,6 +2501,13 @@ multiplicity 2
         self.assertEqual(len(aromatic_atoms), 0)
         self.assertEqual(len(aromatic_bonds), 0)
 
+    def test_aromaticity_perception_benzonaphthalene(self):
+        """Test aromaticity perception via get_aromatic_rings for benzonaphthalene with multiple fused bonds."""
+        mol = Molecule(smiles='c1cc2ccc3ccc(c1)c2c3')
+        aromatic_atoms, aromatic_bonds = mol.get_aromatic_rings()
+        self.assertEqual(len(aromatic_atoms), 1)
+        self.assertEqual(len(aromatic_bonds), 1)
+
     def test_aromaticity_perception_save_order(self):
         """Test aromaticity perception via get_aromatic_rings for phenyl radical without changing atom order."""
         mol = Molecule().from_adjacency_list("""multiplicity 2